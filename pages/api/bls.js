32ee1188938c25ace9b69ca5e08d8275813b338e
ImpostorEngineer <posta.gereksiz@gmail.com>
2022-12-20 12:56:31 -0500

    fix backend for 10-year data pull

--- pages/api/bls.js
+++ pages/api/bls.js
@@ -2,12 +2,14 @@ import axios from 'axios';
 
 async function GetLaborStats() {
   const url = 'https://api.bls.gov/publicAPI/v2/timeseries/data/';
+  const currentYear = new Date().getFullYear();
+  const startYear = currentYear - 10;
   const laborStats = await axios.post(
     url,
     {
       seriesid: ['CES0000000001', 'CES7000000001'],
-      startyear: '2015',
-      endyear: '2022',
+      startyear: startYear,
+      endyear: currentYear,
       registrationkey: process.env.BLS_KEY,
     },
     { headers: { 'content-type': 'application/json' } }
