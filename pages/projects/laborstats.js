724ac2d23269ad26260157b6c4489238f48a8201
ImpostorEngineer <posta.gereksiz@gmail.com>
2023-01-14 13:25:00 -0500

    add cpi chart

--- pages/projects/laborstats.js
+++ pages/projects/laborstats.js
@@ -4,7 +4,7 @@ import utilStyles from '../../styles/utils.module.css';
 import CreateChart from '../../components/apexchartlayout';
 import { useEffect, useState } from 'react';
 
-function createDataArray(data) {
+function createLaborDataArray(data) {
   let dateData = [];
   let employmentData = [];
   let hospitalityEmploymentData = [];
@@ -45,8 +45,35 @@ function percentChange(data) {
   return finalData;
 }
 
+function createCPIData(data) {
+  const cpiRawData = data.Results.series.filter((s) => s.seriesID == 'CUSR0000SA0')[0];
+
+  const cpiChartData = cpiRawData.data.reduce((obj, month, i) => {
+    const previousMonth = cpiRawData[i + 1];
+    if (!obj['date']) {
+      obj['date'] = [`${month['year']}-${month['periodName']}`];
+    } else {
+      obj['date'].unshift([`${month['year']}-${month['periodName']}`]);
+    }
+    if (!obj['cpiValue']) {
+      obj['cpiValue'] = [month['value']];
+    } else {
+      obj['cpiValue'].unshift([month['value']]);
+    }
+    if (!obj['change']) {
+      obj['change'] = [Math.round((month['value'] / previousMonth['value'] - 1) * 1000) / 10];
+    } else {
+      if (previousMonth) {
+        obj['change'].unshift([Math.round((month['value'] / previousMonth['value'] - 1) * 1000) / 10]);
+      }
+    }
+    return obj;
+  }, {});
+  return cpiChartData;
+}
+
 function laborDataChartOptions(data) {
-  const laborData = createDataArray(data);
+  const laborData = createLaborDataArray(data);
 
   const laborChartData = [
     {
@@ -264,6 +291,106 @@ function percentDataChartOptions(data) {
   return { percentLaborChartData, percentLaborChartOptions };
 }
 
+function cpiDataChartOptions(data) {
+  const cpiData = createCPIData(data);
+
+  const cpiChartData = [
+    {
+      name: 'CPI',
+      data: cpiData['cpiValue'],
+    },
+    {
+      name: '% Change',
+      data: cpiData['change'],
+    },
+  ];
+
+  const cpiChartOptions = {
+    theme: {
+      mode: 'dark',
+      palette: 'palette6',
+      monochrome: {
+        enabled: false,
+        color: '#255aee',
+        shadeTo: 'light',
+        shadeIntensity: 0.65,
+      },
+    },
+    chart: {
+      background: '#000',
+      dropShadow: {
+        enabled: true,
+        enabledOnSeries: [0, 1, 2],
+        top: 1,
+        left: 1,
+        blur: 0,
+        color: '#000',
+        opacity: 1,
+      },
+      fontFamily: 'InterVariable, Roboto, Arial, sans-serif',
+      height: 400,
+      width: 800,
+      type: 'line',
+      zoom: {
+        enabled: false,
+      },
+    },
+    colors: ['#d90429', '#ffc300'],
+    fill: {
+      type: 'solid',
+      opacity: [1, 1],
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: 'straight',
+      width: 3,
+    },
+    title: {
+      text: 'CPI',
+      align: 'center',
+      style: {
+        fontWeight: 600,
+        fontSize: '16px',
+      },
+    },
+    xaxis: {
+      categories: cpiData['date'],
+      labels: {
+        rotate: -45,
+        maxHeight: 80,
+      },
+      title: {
+        text: 'Source: BLS',
+        offsetX: -18,
+        offsetY: 140,
+        style: {
+          color: '#9C9C9C',
+          fontSize: '10px',
+          fontFamily: 'InterVariable, Roboto, sans-serif',
+          fontWeight: 400,
+        },
+      },
+      tickAmount: 15,
+    },
+    yaxis: [
+      {
+        seriesName: 'CPI',
+        forceNiceScale: true,
+        decimalsInFloat: 2,
+      },
+      {
+        opposite: true,
+        seriesName: '% Change',
+        forceNiceScale: true,
+        decimalsInFloat: 2,
+      },
+    ],
+  };
+  return { cpiChartData, cpiChartOptions };
+}
+
 export default function LaborStats() {
   const [laborData, setLaborData] = useState(null);
 
@@ -291,6 +418,8 @@ export default function LaborStats() {
 
   const { percentLaborChartData, percentLaborChartOptions } = percentDataChartOptions(laborData);
 
+  const { cpiChartData, cpiChartOptions } = cpiDataChartOptions(laborData);
+
   return (
     <Layout>
       <Head>
@@ -298,6 +427,10 @@ export default function LaborStats() {
       </Head>
       <h2 className={utilStyles.headingLg}>U.S. Employment Data</h2>
       <section>
+        <div>
+          <h3>CPI</h3>
+          <CreateChart data={cpiChartData} options={cpiChartOptions} type={'line'} height={500} />
+        </div>
         <div>
           <h3>Current Employment Numbers:</h3>
           <CreateChart data={laborChartData} options={laborChartOptions} type={'line'} height={500} />
