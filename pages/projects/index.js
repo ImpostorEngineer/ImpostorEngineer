d70ac68290d32a6bcc949df9e2bf8854d42f2f11
ImpostorEngineer <posta.gereksiz@gmail.com>
2024-01-23 12:49:01 -0500

    fix syntax

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -11,41 +11,39 @@ export default function Projects() {
       </Head>
       <h3 className={utilStyles.headingLg}>Some of my Projects:</h3>
       <div className={utilStyles.postBody}>
-        <div>
-          <ul className='has-text-weight-medium'>
-            <li>
-              <Link href='projects/secfilings'>SEC Filings</Link>
-            </li>
-            <li>
-              <Link href='projects/laborstats'>U.S. Economy Charts</Link>
-            </li>
-            <li>
-              <a href='https://impostor-twitch-overlays.vercel.app/' target='_blank'>
-                Impostor Stream Overlays
-              </a>
-            </li>
-            <li>
-              <a href='https://impostor-calculator.vercel.app/' target='_blank'>
-                Mortgage Payoff Calculator
-              </a>
-            </li>
-            <li>
-              <a href='https://runewords.vercel.app/' target='_blank'>
-                Diablo 2 Runewords
-              </a>
-            </li>
-            <li>
-              <a href='https://aoestats.vercel.app/' target='_blank'>
-                Age of Empires II: Definitive Edition Player Statistics
-              </a>
-            </li>
-            <li>
-              <a href='https://github.com/ImpostorEngineer/APODTwitterBot' target='_blank'>
-                A Twitter Bot that posts a random APOD picture every 4 hours.
-              </a>
-            </li>
-          </ul>
-        </div>
+        <ul className='has-text-weight-medium'>
+          <li>
+            <Link href='projects/secfilings'>SEC Filings</Link>
+          </li>
+          <li>
+            <Link href='projects/laborstats'>U.S. Economy Charts</Link>
+          </li>
+          <li>
+            <a href='https://impostor-twitch-overlays.vercel.app/' target='_blank'>
+              Impostor Stream Overlays
+            </a>
+          </li>
+          <li>
+            <a href='https://impostor-calculator.vercel.app/' target='_blank'>
+              Mortgage Payoff Calculator
+            </a>
+          </li>
+          <li>
+            <a href='https://runewords.vercel.app/' target='_blank'>
+              Diablo 2 Runewords
+            </a>
+          </li>
+          <li>
+            <a href='https://aoestats.vercel.app/' target='_blank'>
+              Age of Empires II: Definitive Edition Player Statistics
+            </a>
+          </li>
+          <li>
+            <a href='https://github.com/ImpostorEngineer/APODTwitterBot' target='_blank'>
+              A Twitter Bot that posts a random APOD picture every 4 hours.
+            </a>
+          </li>
+        </ul>
       </div>
     </Layout>
   );
