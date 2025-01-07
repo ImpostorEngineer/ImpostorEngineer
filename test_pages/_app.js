ac1f9f7917d02ce0062cdeadc5c04c47f9251683
Impostor Engineer <posta.gereksiz@gmail.com>
2025-01-07 16:06:47 -0500

    remove some pages

--- test_pages/_app.js
+++
@@ -1,9 +0,0 @@
-import Layout from '../app/layout.js';
-
-export default function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
-}
