e152147a30979ebbcc0b561fdb500433427c7bdc
Impostor Engineer <posta.gereksiz@gmail.com>
2025-01-07 12:35:43 -0500

    problem with theme

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ export default function Layout({ children }) {
         <title>Impostor Engineer</title>
         <meta name='description' content='A blog about software engineering, data science, and other things.' />
       </head>
-      <body className='antialiased max-w-screen-lg mx-4 mt-8 lg:mx-auto'>
+      <body className='antialiased max-w-screen-lg mx-4 mt-8 lg:mx-auto' data-theme='light'>
         <ThemeProvider>
           <Navbar />
           <main className='min-h-96'>{children}</main>
